afcfe1f61b706a7c250b7e469f5002957d82f8b1
Hoang Nguyen <nhsh1997@gmail.com>
2019-06-27 22:51:32 +0700

    update

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router({});
 
 router.use('/users', require('../modules/user/user.route'));
 router.use('/admin/users', require('../modules/user/admin-user.route'));
+router.use('/user-behaviors', require('../modules/user-behavior-log/user-behavior-log.route'));
 
 module.exports = router;
