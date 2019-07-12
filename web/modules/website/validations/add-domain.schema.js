72258e57fadba1f86f695a436c53e5d11d0118ff
Thuan <lanhvietthuan1996@gmail.com>
2019-07-12 07:13:49 +0700

    T003: Refactor code under review (add regex validation for domain).

--- web/modules/website/validations/add-domain.schema.js
+++ web/modules/website/validations/add-domain.schema.js
@@ -1,7 +1,7 @@
 const Joi = require('@hapi/joi');
 
 const AddDomainForAccountAdsValidationSchema = Joi.object().keys({
-    domain: Joi.string().required(),
+    domain: Joi.string().required().regex(/(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/),
     accountId: Joi.string().required().regex(/^[0-9a-fA-F]{24}$/)
   }
 );
