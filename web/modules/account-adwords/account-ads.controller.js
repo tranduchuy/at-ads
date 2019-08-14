b0dac792dad84376bd764415b3a88c209e1f77a5
viettran <Viettran040898.vt@gmail.com>
2019-08-14 09:15:14 +0700

    delete console.log

--- web/modules/account-adwords/account-ads.controller.js
+++ web/modules/account-adwords/account-ads.controller.js
@@ -1173,8 +1173,8 @@ const getIpsInfoInClassD = async (req, res, next) => {
 
     const result = await AccountAdsService.getIpsInfoInClassD(accountKey, from, endDateTime, page, limit);
     let rangeIps = [];
-    console.log(result);
     let totalItems = 0;
+    
     if(result.entries.length !== 0)
     {
       rangeIps = result[0].entries;
