e5cb9ae5f993f9a7fd91c769e98feac4086b68d5
Viettran040898 <viettran040898.vt@gmail.com>
2019-09-04 15:22:18 +0700

    add api get actions history list

--- web/modules/user/user.route.js
+++ web/modules/user/user.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router({});
 const UserController = require('./user.controller');
+const UserActionHistoryController = require('../user-action-history/user-action-history.controller');
 const CheckTokenMiddleware = require('../../middlewares/check-token');
 const multer = require('multer');
 
@@ -15,5 +16,6 @@ router.put('/', CheckTokenMiddleware, UserController.update);
 router.post('/forget-password', UserController.forgetPassword);
 router.post('/reset-password', UserController.resetPassword);
 router.get('/info', CheckTokenMiddleware, UserController.getLoggedInInfo);
+router.get('/actions-history', CheckTokenMiddleware, UserActionHistoryController.getActionsHistory);
 
 module.exports = router;
