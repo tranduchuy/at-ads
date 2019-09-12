75d7448ae3e3e1d91d6b2b90f4c572b699151d19
Viettran040898 <viettran040898.vt@gmail.com>
2019-09-12 13:45:05 +0700

    Fix moethob

--- web/modules/website/website.route.js
+++ web/modules/website/website.route.js
@@ -8,6 +8,6 @@ router.post('/', CheckTokenMiddleware, WebsiteController.addDomainForAccountAds)
 router.get('/', CheckTokenMiddleware, WebsiteController.getWebsitesByAccountId);
 router.put('/:websiteId', CheckTokenMiddleware, WebsiteController.editDomain);
 router.delete('/:code', CheckTokenMiddleware, WebsiteController.deleteDomain);
-router.post('/:code', CheckTokenMiddleware, CheckUserAdminMiddleware, WebsiteController.checkWebsiteByCode);
+router.get('/:code', CheckTokenMiddleware, CheckUserAdminMiddleware, WebsiteController.checkWebsiteByCode);
 
 module.exports = router;
