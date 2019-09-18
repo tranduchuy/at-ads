c146a32f2bee4744263ce35e1727b9b0c17064b1
Viettran040898 <viettran040898.vt@gmail.com>
2019-09-18 15:15:35 +0700

    Update getUsersListForAdminPage

--- web/modules/user/user.route.js
+++ web/modules/user/user.route.js
@@ -3,6 +3,7 @@ const router = express.Router({});
 const UserController = require('./user.controller');
 const UserActionHistoryController = require('../user-action-history/user-action-history.controller');
 const websiteController = require('../website/website.controller');
+const AdminUser = require('./admin-user.controller');
 const CheckTokenMiddleware = require('../../middlewares/check-token');
 const CheckUserAdminMidddleware = require('../../middlewares/check-user-admin');
 const multer = require('multer');
@@ -20,5 +21,6 @@ router.post('/reset-password', UserController.resetPassword);
 router.get('/info', CheckTokenMiddleware, UserController.getLoggedInInfo);
 router.get('/actions-history', CheckTokenMiddleware, UserActionHistoryController.getActionsHistory);
 router.put('/website', CheckTokenMiddleware, CheckUserAdminMidddleware, websiteController.updateDomainToVip);
+router.get('/', CheckTokenMiddleware, CheckUserAdminMidddleware, AdminUser.getUsersListForAdminPage);
 
 module.exports = router;
