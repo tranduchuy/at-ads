ddd200bd07a26da8c81cffe45fdbcf2ae0ade364
Việt Trần <enzoferrari.vt@gmail.com>
2020-01-06 15:37:15 +0700

    Update config popup

--- web/modules/website/validations/update-popup-status-for-website.schema.js
+++ web/modules/website/validations/update-popup-status-for-website.schema.js
@@ -3,7 +3,7 @@ const Joi = require('@hapi/joi');
 const UpdatePopupStatusForWebsiteValidateSchema = Joi.object().keys({
     website: Joi.string().required().regex(/^[0-9a-fA-F]{24}$/),
     popupStatus: Joi.boolean(),
-    popupPosition: Joi.string().valid(['1', '2']),
+    popupPosition: Joi.number().valid([1, 2]),
     autoShowPopupRepeatTime: Joi.number(),
     autoShowPopup: Joi.boolean(),
   }
