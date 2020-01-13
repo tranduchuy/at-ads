086d75819fe3fdf4f4532446f914c80c2c27c565
Việt Trần <enzoferrari.vt@gmail.com>
2020-01-13 15:44:54 +0700

    config header

--- web/app.js
+++ web/app.js
@@ -31,8 +31,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.header('origin')); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin: *');
+	res.header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+	res.header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.header('Access-Control-Allow-Credentials: true');
   next();
 });
 app.use('/api', require('./routes'));
