9cb150efaf2b79e559c05cd077a82900aa53e371
Việt Trần <enzoferrari.vt@gmail.com>
2020-01-14 09:48:13 +0700

    Update fix error

--- web/app.js
+++ web/app.js
@@ -31,10 +31,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin: *');
-	res.header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-	res.header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	res.header('Access-Control-Allow-Credentials: true');
+  res.header('access-Control-Allow-Origin: *');
+	res.header("access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+	res.header('access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.header('access-Control-Allow-Credentials: true');
   next();
 });
 app.use('/api', require('./routes'));
