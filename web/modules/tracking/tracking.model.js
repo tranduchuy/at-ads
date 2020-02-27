aebb8b3e247f22db83b79ed8a393077abc128732
Your Name <you@example.com>
2020-02-27 10:33:21 +0700

    Update tracking

--- web/modules/tracking/tracking.model.js
+++ web/modules/tracking/tracking.model.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const trackingsSchema = new Schema({
-  info: {type: Object, default: null}
+  info: {type: Object, default: null},
+  ip: {type: String, default: null},
+  location: {type: Object, default: null},
+  userAgent: {type: Object, default: null}
 }, { timestamps: true });
 
 const trackingsModel = mongoose.model('Tracking', trackingsSchema, 'Trackings');
