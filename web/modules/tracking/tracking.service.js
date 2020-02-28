3183c58986179a0286f9863c0e3aef16838b594f
Your Name <you@example.com>
2020-02-28 13:35:58 +0700

    Update tracking ads

--- web/modules/tracking/tracking.service.js
+++ web/modules/tracking/tracking.service.js
@@ -1,24 +1,43 @@
 const log4js = require('log4js');
 const logger = log4js.getLogger('Services');
-const request = require('sync-request')
+const requestIp = require('request-ip');
+const geoip = require('geoip-lite');
 
-const getGeoIp = async () => {
+const getGeoIp = (req) => {
   logger.info('TrackingSerives::getGeoIp::is called');
   try{
-    const res = request('GET', 'https://geoip-db.com/json/');
-    const data = JSON.parse(res.getBody())
-    const ip = data.IPv4;
-    let userLocation = data;
-    delete userLocation.IPv4;
+    const ip = requestIp.getClientIp(req);
+    const location = geoip.lookup(ip);
 
-    logger.info('TrackingSerives::getGeoIp::success');
-    return { ip, userLocation };
+    return {ip, location};
   }catch(e){
     logger.error('TrackingSerives::getGeoIp::error', e);
-    throw new Error(e)
+    throw new Error(e);
   }
 };
 
+const detectAdsInfo = (req) => {
+  try{
+    const info = {
+      url: req.query.url || null,
+      campaignId: req.query.click_campaignid || null,
+      keyword: req.query.click_keyword || null,
+      gclid: req.query.click_gclid || null,
+      location: req.query.click_location || null,
+      matchType: req.query.click_matchtype || null,
+      adPosition : req.query.click_adposition || null,
+      network: req.query.click_network || null,
+      gb: req.query.gb || null
+    }
+
+    return info;
+  }catch(e){
+    logger.error('TrackingSerives::getGeoIp::error', e);
+    throw new Error(e);
+  }
+}
+
 module.exports = {
-  getGeoIp
+  getGeoIp,
+  detectAdsInfo
 }
